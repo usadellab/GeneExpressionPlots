6f78181025123a9f5950b3929e61a181f89e350a
David Velasco <davvelsan@gmail.com>
2020-09-02 20:50:12 +0200

    fix: missing home path component

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,15 +2,13 @@ import React, { lazy, Suspense } from 'react';
 import { Route, Switch }         from "react-router-dom";
 
 
-const GroupList   = lazy(() => import('./views/GroupList'));
-
 export default function AppRoutes () {
 
   return (
     <Suspense fallback={ <div>Loading...</div> }>
 
       <Switch>
-        <Route exact path="/" component={ GroupList } />
+        <Route exact path="/" component={ () => <div></div> } />
       </Switch>
 
     </Suspense>
