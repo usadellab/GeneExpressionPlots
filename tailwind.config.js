eb32457bb767f7db791fc679f6df8703079fbb21
David Velasco <davvelsan@gmail.com>
2020-09-20 13:59:24 +0200

    styles: tweaks to color palette (closer to material)

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,44 @@ module.exports = {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        'outer': '0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)'
+      },
+      colors: {
+        gray: {
+          '100': '#fdfdfd',
+          '200': '#f6f6f6',
+          '300': '#eaeaea',
+          '400': '#d6d6d6',
+          '500': '#bcbcbc',
+          '600': '#9b9b9b',
+          '700': '#767676',
+          '800': '#505050',
+          '900': '#272727',
+        },
+        blue: {
+          '100': '#fbfdff',
+          '200': '#eff7ff',
+          '300': '#daecff',
+          '400': '#b7daff',
+          '500': '#87c1ff',
+          '600': '#439eff',
+          '700': '#0072ec',
+          '800': '#004ea1',
+          '900': '#002750',
+        },
+        pink: {
+          '100': '#fffcfd',
+          '200': '#fff3f7',
+          '300': '#ffe3ed',
+          '400': '#ffc7db',
+          '500': '#ffa1c2',
+          '600': '#ff659b',
+          '700': '#ea0052',
+          '800': '#a20039',
+          '900': '#55001e',
+        }
+      },
       fontFamily: {
         abeeze: [
           'ABeeZee', 'system-ui', '-apple-system', 'BlinkMacSystemFont', "Segoe UI",
