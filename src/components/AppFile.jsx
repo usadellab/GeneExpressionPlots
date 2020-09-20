defde2c8113304944172104870cddccffda1d15a
David Velasco <davvelsan@gmail.com>
2020-09-20 19:42:49 +0200

    feat: data storage drawer component

---
+++ src/components/AppFile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import AppRipple from './AppRipple';
+
+
+/**
+ * @typedef  {Object} AppFileProps Properties object for the AppFile component.
+ * @property {string} className css classes to apply in the root component
+ *
+ * @param {AppFileProps} props component properties
+ */
+export default function AppFile (props) {
+
+  const { className, children, ...inputProps } = props;
+
+  return (
+    <label className={
+      `relative overflow-hidden
+       flex items-center py-2 px-5 cursor-pointer
+       rounded focus:outline-none font-medium uppercase
+       ${className}`
+    }>
+      <input className="hidden" type="file" { ...inputProps } />
+
+      { children }
+
+      <AppRipple />
+
+    </label>
+  );
+}
---
+++ src/modules/data/components/DataStorageDrawer.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import AppDrawer from '@components/AppDrawer';
+import AppFile   from '@components/AppFile';
+
+import { useDataStore } from '../store';
+
+
+/**
+ * @typedef {Object} DataStorageProps Properties object for the DataStorage component.
+ * @property {() => void} onLoad callback after loading files
+ *
+ * @param {DataStorageProps} props component properties
+ */
+export default function DataStorage (props) {
+
+  const { show, setShow } = props;
+
+  const { dispatch } = useDataStore();
+
+  /**
+   * Appends the groups of an exported data file to the current store.
+   * @param {React.ChangeEvent<HTMLInputElement>} event file input event
+   */
+  const handleLoadFile = (event) => {
+
+    const file = event.target.files.item(0);
+
+    if (!file || file.type !== 'application/json') return;
+
+    const fr = new FileReader();
+
+    fr.readAsText(file, 'utf-8');
+
+    fr.onload = () => {
+
+      const value = JSON.parse(fr.result);
+
+      dispatch({
+        type: 'CREATE',
+        payload: {
+          value,
+        },
+      });
+
+      setShow(false);
+    };
+  };
+
+  return (
+    <AppDrawer
+      className="w-1/2"
+      show={ show }
+      setShow={ setShow }
+    >
+      <AppFile
+        className="flex justify-center m-2 primary-blue"
+        onChange={ handleLoadFile }
+      >
+      Load File
+      </AppFile>
+    </AppDrawer>
+  );
+}
