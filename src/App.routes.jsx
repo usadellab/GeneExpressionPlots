c57005050a65a2b73e6190c7e9442b94ee6107be
cons <cons@pop-os.localdomain>
2020-09-21 18:58:07 +0200

    WIP: PLotly component kinda working, I guess?

--- src/App.routes.jsx
+++ src/App.routes.jsx
@@ -1,13 +1,17 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-const defaultHome = lazy(() => import('./modules/default/Home'));
+// const defaultHome = lazy(() => import('./modules/default/Home'));
+const plotly = lazy(() => import('./modules/plotly/Plotly'));
+// import plotly from './modules/plotly/Plotly';
+
 
 export default function AppRoutes() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
-        <Route exact path="/" component={defaultHome} />
+        {/* <Route exact path="/" component={defaultHome} /> */}
+        <Route exact path="/" component={plotly} />
       </Switch>
     </Suspense>
   );
