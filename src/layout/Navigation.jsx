97a2ba888719b600347ab6769f3f1ecb26ed5cdb
cons <cons@pop-os.localdomain>
2020-10-16 13:32:05 +0200

    feat: disable Plot functionality if no data - make Navigation component observable - conditionally add css to MenuComponet to disable

--- src/layout/Navigation.jsx
+++ src/layout/Navigation.jsx
@@ -7,6 +7,7 @@ import AppIcon   from '@components/AppIcon';
 import AppFile   from '@components/AppFile';
 
 import { store } from '@/store';
+import { observer } from 'mobx-react';
 
 
 const NavMenu = (props) => {
@@ -22,8 +23,11 @@ const NavMenu = (props) => {
   return (
     <li key={`${props.name}`} >
       <MenuComponent
-        className="flex items-center py-2 cursor-pointer
-                   text-gray-800 hover:text-blue-600 text-base capitalize font-bold"
+        className= {
+          `flex items-center py-2 cursor-pointer
+                   text-gray-800 hover:text-blue-600 text-base capitalize font-bold ${props.disabled ? 'opacity-50 cursor-not-allowed' : ''}`
+        } 
+                    
         { ...props }
       >
         { props.icon && <AppIcon file="base" id={ props.icon } className="w-6 h-6 mr-2" /> }
@@ -53,6 +57,7 @@ const NavSection = (props) => (
   </div>
 );
 
+@observer
 export default class Sidebar extends React.Component {
 
   constructor() {
@@ -100,6 +105,10 @@ export default class Sidebar extends React.Component {
 
   handleClearPlots = () => store.clearPlots();
   handleClearData = () => store.clearData();
+
+  get storeHasData() {
+    return store.groups.length === 0 ? true : false;
+  }
 
   render () {
     return (
@@ -167,6 +176,7 @@ export default class Sidebar extends React.Component {
             icon="hi-chart-square-bar"
             name="New Plot"
             onClick={ this.props.showPlotsModal }
+            disabled={ this.storeHasData }
           />
 
           <NavMenu
@@ -174,6 +184,7 @@ export default class Sidebar extends React.Component {
             icon="hi-trash"
             name="Clear Plots"
             onClick={ this.handleClearPlots }
+            disabled={ this.storeHasData }
           />
 
         </NavSection>
