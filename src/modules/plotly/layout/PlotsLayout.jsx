634cb4a1ac103d16eb6c7cd43396db4cae96cfc1
David Velasco <davvelsan@gmail.com>
2020-10-24 13:56:17 +0200

    feat: better preloaded image size and media query responsiveness

--- src/modules/plotly/layout/PlotsLayout.jsx
+++ src/modules/plotly/layout/PlotsLayout.jsx
@@ -33,26 +33,44 @@ export default class PlotsLayout extends React.Component {
 
         {/* MAIN CONTENT: ROUTES */}
 
-        <main className="flex flex-wrap p-6 mt-10 w-full h-full" >
+        <main
+          className="flex flex-col items-center mt-10 p-6 w-full h-full
+                     xl:flex-row xl:flex-wrap"
+        >
 
           {/* PRELOADED IMAGE
 
-            This <img/> element comes pre-styled to integrate with the existing layout as
-            best as possible. To do this, it makes some sensible assumptions about how the
-            image should likely be displayed.
+            This <img/> element is pre-styled to integrate with the existing layout as
+            best as possible. To do this, we make some sensible assumptions about how the
+            image is most likely going to be displayed in relation to the plots.
 
-            The current layout is a flex display with automatic wrapping, where each element
-            uses half the <main> container width.
+            - The layout in small screens (up to width: 768px) is a flex display with column
+            orientation. Each <Plot/> element will use the full width of the <main> container.
 
-            The <img/> element is positioned as if it were another plot in the layout. A
-            padding of 6rem units is also added to the <img/> element to emulate the default
-            positioning of the <Plot/> element as produced by Plotly.js.
+            - The layout in large screens (width: 1440px) is a flex display with automatic
+             wrapping. Each <Plot/> element will use half of the <main> container width.
+
+            Considering the points above, the <img/> element is positioned as if it were another
+            <Plot/>, with a few differences:
+
+            - The image container is a <div/> element with a full width, and a padding percentage
+            base on that full width. This creates a fluid container that will resize along with
+            the screen width.
+
+            - The image container width is limited to a maximum size in screens up to laptop
+            size (width: 1280px). After that, the image width will always take up 50% of the
+            <main> element, like the plots.
+
+            These <img/> styles try to emulate the way Plotly.js renders <Plot/> elements, while
+            at same time keeping the image locked to an aspect ratio.
+
+            Source: https://tailwindcss.com/course/locking-images-to-a-fixed-aspect-ratio
 
           */}
           {
             this.state.img &&
-            <div className="flex justify-center items-center w-full xl:w-1/2">
-              <img className="p-24" src={ this.state.img } />
+            <div className="relative w-full pb-1/2 sm:max-w-md lg:max-w-lg xl:w-1/2">
+              <img className="absolute w-full h-full object-contain" src={ this.state.img } />
             </div>
           }
 
