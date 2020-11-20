6ed2051e39323d85f550cb25b59103c25cd8ff2f
David Velasco <davvelsan@gmail.com>
2020-11-20 13:24:07 +0100

    fix: adjust data card colors to match the new grey background

--- src/modules/data/layout/DataLayout.jsx
+++ src/modules/data/layout/DataLayout.jsx
@@ -6,7 +6,7 @@ export default class HomeLayout extends React.Component {
   render () {
 
     return (
-      <main className="flex justify-center mt-10 mr-6 px-6 md:px-12 w-full" >
+      <main className="flex justify-center mr-6 py-10 px-6 md:px-12 w-full" >
 
         { this.props.children }
 
