d5a72afc54f52e7f9c3d1d458235c46f11c25bd7
David Velasco <davvelsan@gmail.com>
2020-12-09 19:53:33 +0100

    refactor: extract the PlotCaption component to its own file refactor: rename AppPlot->PlotlyPlot refactor: add a children property to PlotlyPlot that is assumed to be a PlotCaption feat: replace the captions input object with a new table object BREAKING CHANGES: - The captions input is no longer a JSON file with { key: description } pairs, but a  tabular input in which the first column is assumed to be the accession id, and the remaining columns arbitrary field-values

--- src/modules/plotly/pages/PlotsView.jsx
+++ src/modules/plotly/pages/PlotsView.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import AppPlot from '@components/AppPlot';
+import PlotlyPlot from '../components/PlotlyPlot';
+import PlotCaption from '../components/PlotCaption';
 
 import { store } from '@/store';
 
 @observer
-export default class PlotlyComponent extends React.Component {
+export default class PlotsView extends React.Component {
   render() {
     return (
       store.plots.length > 0 &&
       store.plots.map((plot, index) => (
-        <AppPlot
+        <PlotlyPlot
           key={`${plot?.layout?.title?.text}-${index}`}
-          className="relative flex flex-col m-3 py-6 w-full resize-x shadow-outer overflow-auto bg-white"
+          className="relative flex flex-col m-3 py-6 w-full resize-x
+                     shadow-outer overflow-auto bg-white"
           plot={{ ...plot }}
-          showCaption={plot.showCaption}
-          accessions={plot.accessions}
-          captions={plot.accessions.map((accession) => {
-            return store.captions[accession]
-              ? store.captions[accession]
-              : 'no gene caption available';
-          })}
-        />
+          showCaption={ plot.showCaption }
+        >
+          {
+            plot.showCaption &&
+            plot.accessions.map(accession => (
+              <PlotCaption
+                key={ accession }
+                accession={ accession }
+                caption={ store.getCaption(accession) }
+              />
+            ))
+          }
+        </PlotlyPlot>
       ))
     );
   }
