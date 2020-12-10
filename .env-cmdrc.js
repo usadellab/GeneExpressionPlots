c571daa118bb04461c8bef59e7b8b1ccd803c7dd
David Velasco <davvelsan@gmail.com>
2020-12-10 16:06:55 +0100

    chore: use JS for env-cmdrc

---
+++ .env-cmdrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    APP_NAME: 'DEV_GEP',
+    BASE_URL: '/',
+    NODE_ENV: 'development',
+  },
+  production: {
+    APP_NAME: 'GeneExpPlots',
+    BASE_URL: '/GeneExpressionPlots',
+    NODE_ENV: 'production',
+  }
+}
