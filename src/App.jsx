0c0e51d9de05f3afca5d816f3996912b9678361d
David Velasco <davvelsan@gmail.com>
2020-12-10 12:55:46 +0100

    refactor: remove captions preload from json data

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,13 @@ import AppLayout from './layout/AppLayout';
 
 import { store } from '@/store';
 import {
+  PRELOAD_CAPTIONS,
   PRELOAD_DATA,
   PRELOAD_IMAGE,
 } from './config/globals.js';
+
+import { fetchResource } from '@/utils/fetch';
+import { readTable } from '@/utils/parser';
 
 import '@/assets/svg/hero-icons.svg';
 
@@ -16,40 +20,29 @@ import '@/assets/svg/hero-icons.svg';
 export default class App extends React.Component {
 
   async componentDidMount () {
+
+    let groups = [];
+    let captions = {};
+    let image = null;
+
     if (PRELOAD_DATA) {
-      try {
-        const response = await fetch(PRELOAD_DATA);
-        if (response.ok) {
-          const { data, captions, image } = await response.json();
-          if (data)
-            store.assignData(data);
-          else
-            throw new Error('Loading preloaded data caused an error: no data found');
-          if (captions)
-            store.assignCaptions(captions);
-          if (image)
-            store.assignImage(image);
-        }
-        else
-          console.error('Loading preloaded data caused an error');
-      }
-      catch (error) {
-        console.error(error.message);
-      }
+      const dataRes = await fetchResource(PRELOAD_DATA, { type: 'json' });
+      if (dataRes?.data) groups = dataRes.data;
+      if (dataRes?.image) image = dataRes.image;
+    }
+
+    if (PRELOAD_CAPTIONS) {
+      const captionsResponse = await fetchResource(PRELOAD_CAPTIONS, { type: 'text' });
+      if (captionsResponse) captions = readTable(captionsResponse, { fieldSeparator: '\t' });
     }
 
     if (PRELOAD_IMAGE) {
-      try {
-        const response = await fetch(PRELOAD_IMAGE);
-        if (response.ok)
-          store.assignImage(response.url);
-        else
-          console.error('Loading preloaded image caused an error');
-      }
-      catch (error) {
-        console.error(error.message);
-      }
+      image = await fetchResource(PRELOAD_IMAGE, { type: 'url' });
     }
+
+    store.assignCaptions(captions);
+    store.assignImage(image);
+    store.assignData(groups);
   }
 
   render () {
--- src/config/globals.js
+++ src/config/globals.js
@@ -1,2 +1,3 @@
-export const PRELOAD_DATA     = process.env.PRELOAD_DATA;
-export const PRELOAD_IMAGE    = process.env.PRELOAD_IMAGE;
+export const PRELOAD_CAPTIONS = process.env.PRELOAD_CAPTIONS;
+export const PRELOAD_DATA = process.env.PRELOAD_DATA;
+export const PRELOAD_IMAGE = process.env.PRELOAD_IMAGE;
