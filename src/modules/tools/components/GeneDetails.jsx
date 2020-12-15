1292ad033ea905fc2b6da0fcf6d91c511267f18c
David Velasco <davvelsan@gmail.com>
2020-12-15 15:32:51 +0100

    feat: add header row to gene-details

--- src/modules/tools/components/GeneDetails.jsx
+++ src/modules/tools/components/GeneDetails.jsx
@@ -36,7 +36,13 @@ export default class GeneDetails extends Component {
 
   render() {
     return (
-      <div className="flex flex-col flex-auto max-h-3xl overflow-auto">
+      <div className="flex flex-col flex-auto overflow-hidden overflow-y-scroll">
+        <div className="flex p-5 font-semibold bg-yellow-200 uppercase">
+          <div className="px-5 w-2/6">Group</div>
+          <div className="px-5 w-2/6">Sample</div>
+          <div className="px-5 w-1/6 text-center">Replicate</div>
+          <div className="px-5 w-1/6 text-center">Count</div>
+        </div>
         {
           this.geneDetails &&
           this.geneDetails.map(({ group, sample, replicate, count }) => (
