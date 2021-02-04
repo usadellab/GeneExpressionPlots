64075467e6a9e0d5816fd0b33411de675ba48044
David Velasco <davvelsan@gmail.com>
2021-02-04 18:30:36 +0100

    feat: gene card (#46)
    * fix: gene info layout in figure captions and gene-browser
    refactor: reuse a gene-card to display gene info
    
    * fix: plot-caption key should include accession
    
    * feat: use a row layout for gene-cards in figure captions

--- src/modules/plotly/components/PlotCaption.jsx
+++ src/modules/plotly/components/PlotCaption.jsx
@@ -1,61 +1,41 @@
 import React from 'react';
 
+import GeneCard from '@/components/cards/GeneCard';
 import { PlotContext } from './PlotlyPlot';
 
 export default class PlotCaption extends React.Component {
   render() {
     return (
       <PlotContext.Consumer>
-        {
-          ({ hoveredGene }) => (
-            <figcaption
-              className={
-                `flex flex-col py-5 mt-2
-                 border-t-2 hover:border-yellow-600
-                 hover:bg-yellow-100
-                 ${hoveredGene && !hoveredGene.includes(this.props.accession) ? 'opacity-50' : ''}
-                 ${hoveredGene.includes(this.props.accession) ? 'bg-yellow-100' : ''}
-                 text-justify text-gray-800 text-sm`
-              }
-              style={{ borderColor: this.props.color ?? '' }}
-            >
-
-              <div
-                className="font-semibold text-base"
-                style={{ color: this.props.color ?? '' }}
-              >
-                { this.props.accession }
-              </div>
-
-              <div className="flex items-center mt-2">
-
-                <div>
-                  {
-                    [ ...this.props.caption.keys() ].map(colName => (
-                      <div
-                        key={ colName }
-                        className="ml-2 py-1 uppercase text-yellow-700"
-                      >
-                        { colName }
-                      </div>
-                    ))
-                  }
-                </div>
-
-                <div>
-                  {
-                    [ ...this.props.caption.values() ].map((cellValue, index) => (
-                      <div key={ index } className="ml-5 py-1">
-                        { cellValue }
-                      </div>
-                    ))
-                  }
-                </div>
-
-              </div>
-            </figcaption>
-          )
-        }
+        {({ hoveredGene }) => (
+          <figcaption
+            className={`
+                 flex py-5
+                 hover:bg-yellow-100 odd:bg-gray-100
+                 text-justify text-gray-800 text-sm ${
+                   this.props.className ?? ''
+                 }
+                 ${
+                   hoveredGene && !hoveredGene.includes(this.props.accession)
+                     ? 'opacity-50'
+                     : ''
+                 }
+                 ${
+                   hoveredGene.includes(this.props.accession)
+                     ? 'bg-yellow-100'
+                     : ''
+                 }
+                 `}
+            style={{ borderColor: this.props.color ?? '' }}
+          >
+            <GeneCard
+              className="px-2"
+              accession={this.props.accession}
+              geneInfo={this.props.caption}
+              color={this.props.color}
+            />
+          </figcaption>
+        )}
       </PlotContext.Consumer>
     );
   }
