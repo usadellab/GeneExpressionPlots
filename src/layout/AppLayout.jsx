7ba760336c276187c6b359e0245135e3ebab702c
David Velasco <davvelsan@gmail.com>
2021-02-04 12:05:29 +0100

    feat: layout fixes (#45)
    * feat: use the default serif font
    
    * feat: adjust breakpoints to better support small screens

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,152 +1,137 @@
 import React from 'react';
 
-import AppModal   from '@components/AppModal2';
-import GroupForm  from '@/modules/data/components/GroupForm';
-import TableForm  from '@/modules/data/components/TableForm';
-import InfoForm   from '@/modules/data/components/InfoForm';
-import PlotsForm  from '@/modules/plotly/components/PlotsForm';
-import TopBar     from './TopBar';
+import AppModal from '@components/AppModal2';
+import GroupForm from '@/modules/data/components/GroupForm';
+import TableForm from '@/modules/data/components/TableForm';
+import InfoForm from '@/modules/data/components/InfoForm';
+import PlotsForm from '@/modules/plotly/components/PlotsForm';
+import TopBar from './TopBar';
 import Navigation from './Navigation';
 
-
 export default class AppLayout extends React.Component {
-
-  constructor () {
+  constructor() {
     super();
     this.state = {
       showNav: false,
       showGroupModal: false,
       showPlotsModal: false,
       showTableModal: false,
-      showInfoModal: false
+      showInfoModal: false,
     };
   }
 
   /* NAVIGATION */
 
-  onTopBarToggle = () => this.setState(state => ({ showNav: !state.showNav }))
-  onNavigationClick = () => this.setState({ showNav: false })
+  onTopBarToggle = () =>
+    this.setState((state) => ({ showNav: !state.showNav }));
+  onNavigationClick = () => this.setState({ showNav: false });
 
   /* MODALS */
 
   onShowGroupModal = () => this.setState({ showGroupModal: true });
   onShowPlotsModal = () => this.setState({ showPlotsModal: true });
   onShowTableModal = () => this.setState({ showTableModal: true });
-  onShowInfoModal  = () => this.setState({ showInfoModal: true })
+  onShowInfoModal = () => this.setState({ showInfoModal: true });
 
-  onGroupFormModalHide = () => this.setState({ showGroupModal: false })
-  onGroupFormCancel = () => this.setState({ showGroupModal: false })
-  onGroupFormSave = () => this.setState({ showGroupModal: false })
+  onGroupFormModalHide = () => this.setState({ showGroupModal: false });
+  onGroupFormCancel = () => this.setState({ showGroupModal: false });
+  onGroupFormSave = () => this.setState({ showGroupModal: false });
 
-  onPlotFormModalHide = () => this.setState({ showPlotsModal: false })
-  onPlotFormCancel = () => this.setState({ showPlotsModal: false })
+  onPlotFormModalHide = () => this.setState({ showPlotsModal: false });
+  onPlotFormCancel = () => this.setState({ showPlotsModal: false });
 
-  onTableFormModalHide = () => this.setState({ showTableModal: false })
-  onTableFormCancel = () => this.setState({ showTableModal: false })
-  onTableFormSave = () => this.setState({ showTableModal: false })
+  onTableFormModalHide = () => this.setState({ showTableModal: false });
+  onTableFormCancel = () => this.setState({ showTableModal: false });
+  onTableFormSave = () => this.setState({ showTableModal: false });
 
-  onInfoFormModalHide = () => this.setState({ showInfoModal: false })
-  onInfoFormCancel = () => this.setState({ showInfoModal: false })
-  onInfoFormSave = () => this.setState({ showInfoModal: false })
+  onInfoFormModalHide = () => this.setState({ showInfoModal: false });
+  onInfoFormCancel = () => this.setState({ showInfoModal: false });
+  onInfoFormSave = () => this.setState({ showInfoModal: false });
 
-
-  render () {
+  render() {
     return (
       <>
         {/* NAVIGATION */}
         <div
-          className="relative z-20 py-4 px-3 md:w-64
-                     shadow-xl bg-white
-                     md:fixed md:block md:inset-y-0 md:left-0"
+          className="relative z-20 py-4 px-3 lg:w-68
+                     shadow-xl bg-white lg:overflow-y-auto
+                     lg:fixed lg:block lg:inset-y-0 lg:left-0"
         >
-
           {/* NAVIGATION HEADER */}
           <TopBar
-            onToggle={ this.onTopBarToggle }
-            show={ this.state.showNav }
+            onToggle={this.onTopBarToggle}
+            show={this.state.showNav}
             brand="Gene Expression Plots"
           />
 
           {/* COLLAPSIBLE NAVIGATION */}
           <Navigation
-            show={ this.state.showNav }
-            showGroupModal={ this.onShowGroupModal }
-            showPlotsModal={ this.onShowPlotsModal }
-            showTableModal={ this.onShowTableModal }
-            showInfoModal={ this.onShowInfoModal }
-            onClick={ this.onNavigationClick }
+            show={this.state.showNav}
+            showGroupModal={this.onShowGroupModal}
+            showPlotsModal={this.onShowPlotsModal}
+            showTableModal={this.onShowTableModal}
+            showInfoModal={this.onShowInfoModal}
+            onClick={this.onNavigationClick}
           />
-
         </div>
 
         {/* PAGE CONTENT */}
-        <div className="relative md:ml-64 flex flex-col min-h-screen bg-gray-300">
-
+        <div className="relative lg:ml-68 flex flex-col min-h-screen bg-gray-300">
           {/* ROUTES */}
-          { this.props.children }
+          {this.props.children}
 
           {/* DATA GROUP FORM */}
-          {
-            this.state.showGroupModal &&
+          {this.state.showGroupModal && (
             <AppModal
               title="Add Replicate Tables"
-              showModal={ this.state.showGroupModal }
-              hideModal={ this.onGroupFormModalHide }
+              showModal={this.state.showGroupModal}
+              hideModal={this.onGroupFormModalHide}
             >
               <GroupForm
-                onSave={ this.onGroupFormSave }
-                onCancel={ this.onGroupFormCancel }
+                onSave={this.onGroupFormSave}
+                onCancel={this.onGroupFormCancel}
               />
             </AppModal>
-          }
+          )}
 
           {/* DATA TABLE FORM */}
-          {
-            this.state.showTableModal &&
+          {this.state.showTableModal && (
             <AppModal
               title="Add Expression Table"
-              showModal={ this.state.showTableModal }
-              hideModal={ this.onTableFormModalHide }
+              showModal={this.state.showTableModal}
+              hideModal={this.onTableFormModalHide}
             >
               <TableForm
-                onSave={ this.onTableFormSave }
-                onCancel={ this.onTableFormCancel }
+                onSave={this.onTableFormSave}
+                onCancel={this.onTableFormCancel}
               />
             </AppModal>
-          }
+          )}
 
           {/* INFO TABLE FORM */}
-          {
-            this.state.showInfoModal &&
+          {this.state.showInfoModal && (
             <AppModal
               title="Add Info Table"
-              showModal={ this.state.showInfoModal }
-              hideModal={ this.onInfoFormModalHide }
+              showModal={this.state.showInfoModal}
+              hideModal={this.onInfoFormModalHide}
             >
               <InfoForm
-                onSave={ this.onInfoFormSave }
-                onCancel={ this.onInfoFormCancel }
+                onSave={this.onInfoFormSave}
+                onCancel={this.onInfoFormCancel}
               />
             </AppModal>
-          }
+          )}
 
           {/* PLOTS FORM */}
-          {
-            this.state.showPlotsModal
-              ?
-              <AppModal
-                title="Add Plot"
-                showModal={ this.state.showPlotsModal }
-                hideModal={ this.onPlotFormModalHide }
-              >
-                <PlotsForm
-                  onCancel={ this.onPlotFormCancel }
-                />
-              </AppModal>
-              :
-              null
-          }
-
+          {this.state.showPlotsModal ? (
+            <AppModal
+              title="Add Plot"
+              showModal={this.state.showPlotsModal}
+              hideModal={this.onPlotFormModalHide}
+            >
+              <PlotsForm onCancel={this.onPlotFormCancel} />
+            </AppModal>
+          ) : null}
         </div>
       </>
     );
