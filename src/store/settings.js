b8abeb9205b637bb9f62939eede2f43e764192ca
David Velasco <davvelsan@gmail.com>
2021-06-10 13:34:43 +0200

    fix: use VITE_ prefix for importing env variables

--- src/store/settings.js
+++ src/store/settings.js
@@ -1,10 +1,17 @@
 import { makeAutoObservable } from 'mobx';
 import { dataTable } from '@/store/data-store';
 
-const PRELOAD_DATA = import.meta.env.PRELOAD_DATA;
-const PRELOAD_INFO = import.meta.env.PRELOAD_INFO;
-const PRELOAD_IMAGE = import.meta.env.PRELOAD_IMAGE;
-const PRELOAD_SETTINGS = import.meta.env.PRELOAD_SETTINGS;
+const PRELOAD_DATA = import.meta.env.VITE_PRELOAD_DATA;
+const PRELOAD_IMAGE = import.meta.env.VITE_PRELOAD_IMAGE;
+const PRELOAD_INFO = import.meta.env.VITE_PRELOAD_INFO;
+const PRELOAD_SETTINGS = import.meta.env.VITE_PRELOAD_SETTINGS;
+
+console.log({
+  PRELOAD_DATA,
+  PRELOAD_IMAGE,
+  PRELOAD_INFO,
+  PRELOAD_SETTINGS,
+});
 
 class Settings {
 
