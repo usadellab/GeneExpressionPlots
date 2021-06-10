c73b7694e494d7737870785ed7ccde62392c1bed
David Velasco <davvelsan@gmail.com>
2021-06-10 21:08:02 +0200

    feat: Cypress testing (#73)
    * chore: port tests from jest to cypress
    fix: ensure all tests are passing
    
    * fix: missing updated yarn.lock file
    
    * chore: add create-cypress-tests helper
    
    * chore: support for component testing using vite-dev-server
    
    * refactor: separate integration (standard) tests from component (ct) tests
    
    * chore: ignore nyc coverage reports
    
    * docs: update README with testing commands and example files docs

---
+++ cypress/tests/components/AppAnchor.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { mount } from '@cypress/react';
+import AppAnchor from '../../../src/components/AppAnchor';
+
+it('renders learn react link', () => {
+  mount(<AppAnchor href="/test/path">Gene Expression Plots</AppAnchor>);
+  cy.get('a').contains('Gene Expression Plots');
+});
