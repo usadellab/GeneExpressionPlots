5ad6143eaf302990fa8a3cabba25fc829d07a627
David Velasco <davvelsan@gmail.com>
2021-06-14 17:41:30 +0200

    feat: support overriding FlexProps in topbar

--- src/components/nav-topbar/topbar-nav.tsx
+++ src/components/nav-topbar/topbar-nav.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
-import { Flex } from '@chakra-ui/react';
+import { Flex, FlexProps } from '@chakra-ui/react';
 import TopbarLink, { TopbarLinkProps } from './topbar-link';
 
-interface TopbarNavProps {
+interface TopbarNavProps extends FlexProps {
   /** @type add a top border accent to the topbar */
   accent?: boolean;
   /** @type object with items to be rendered as links */
   links: Omit<TopbarLinkProps, 'showText'>[];
 }
 
-const TopbarNav: React.FC<TopbarNavProps> = (props): React.ReactElement => {
+const TopbarNav: React.FC<TopbarNavProps> = ({
+  accent,
+  links,
+  ...props
+}): React.ReactElement => {
   return (
     <Flex
       justifyContent="space-around"
       width="100%"
       paddingY={2}
       paddingX={5}
-      borderTop={props.accent ? '8px' : 'none'}
+      borderTop={accent ? '8px' : 'none'}
       borderColor="orange.600"
       backgroundColor="white"
       shadow="lg"
+      {...props}
     >
-      {props.links?.map((link) => (
+      {links?.map((link) => (
         <TopbarLink
           key={link.href}
           href={link.href}
