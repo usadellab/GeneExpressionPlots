b7ef180cceacf8f047586857b77687c949aeea61
David Velasco <davvelsan@gmail.com>
2021-06-15 16:00:58 +0200

    feat: make sidebar-link accessible to kb navigation - add tabIndex prop - remove superfluous flex child

--- src/components/nav-sidebar/sidebar-link.tsx
+++ src/components/nav-sidebar/sidebar-link.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { IconType } from 'react-icons';
 import { useLocation } from 'react-router-dom';
-import { Flex, Icon, Text } from '@chakra-ui/react';
+import { Icon, Text } from '@chakra-ui/react';
 import SiteLink from '@/components/site-link';
 
 export interface SidebarLinkProps {
@@ -25,22 +25,27 @@ const SidebarLink: React.FC<SidebarLinkProps> = (props) => {
 
   return (
     <SiteLink
-      to={props.href}
-      textColor={match ? 'orange.600' : 'gray.600'}
+      _focus={{
+        outline: 'none',
+        border: '1px',
+        borderColor: 'orange.600',
+      }}
       _hover={{
         backgroundColor: 'orange.600',
         textColor: 'white',
       }}
-      _focus={{
-        outline: 'none',
-      }}
+      alignItems="center"
+      display="flex"
+      paddingX={6}
+      paddingY={4}
+      textColor={match ? 'orange.600' : 'gray.600'}
+      tabIndex={0}
+      to={props.href}
     >
-      <Flex paddingX={6} paddingY={4} alignItems="center">
-        <Icon as={props.icon} width={6} height={6} />
-        <Text fontWeight="semibold" marginLeft={8} fontSize="lg">
-          {props.text}
-        </Text>
-      </Flex>
+      <Icon as={props.icon} height={6} width={6} />
+      <Text fontWeight="semibold" marginLeft={6} whiteSpace="nowrap">
+        {props.text}
+      </Text>
     </SiteLink>
   );
 };
