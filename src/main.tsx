2b5beb3ab96d1165f3d478efc3a5e26a49964d99
David Velasco <davvelsan@gmail.com>
2021-06-16 13:59:52 +0200

    chore: disable tailwind and plotly css files

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './app';
 
-import './css/tailwind.css';
-import './css/plotly.css';
+// import './css/tailwind.css';
+// import './css/plotly.css';
 
 ReactDOM.render(
   <React.StrictMode>
