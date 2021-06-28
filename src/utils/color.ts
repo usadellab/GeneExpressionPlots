ac33036b68a60df526ee4ad02d7a2c17c926a430
coeit <cbeiteneuer@gmail.com>
2021-06-28 12:41:41 +0200

    fix: setToColor utility. Object.fromEntries takes zipped Array

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -2,9 +2,8 @@ import iwanthue from 'iwanthue';
 
 export function setToColors(set: Set<string>): Record<string, string> {
   const colors = iwanthue(set.size);
-  const colorMap: Record<string, string> = Object.fromEntries([
-    [...set.values()],
-    colors,
-  ]);
+  const values = Array.from(set.values());
+  const valueColorZip = values.map((value, index) => [value, colors[index]]);
+  const colorMap: Record<string, string> = Object.fromEntries(valueColorZip);
   return colorMap;
 }
