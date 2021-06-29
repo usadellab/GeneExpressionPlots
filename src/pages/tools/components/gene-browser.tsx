60f9518a776e0a3cbac87ac287ac117e6f00a2c9
David Velasco <davvelsan@gmail.com>
2021-06-29 11:15:56 +0200

    feat: add gene-details table and modal

--- src/pages/tools/components/gene-browser.tsx
+++ src/pages/tools/components/gene-browser.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useBreakpointValue,
+  useDisclosure,
+} from '@chakra-ui/react';
 
 import GeneCard from './gene-browser-card';
+import GeneDetails from './gene-details';
 import { dataTable, infoTable } from '@/store/data-store';
-// import { settings } from '@/store/settings';
 
 import { escapeRegExp } from '@/utils/string';
 import GeneBrowserForm, { BrowserFormSubmitHandler } from './gene-browser-form';
@@ -13,26 +25,38 @@ interface GeneCard {
   geneInfo: Map<string, string>;
 }
 
+interface PageView {
+  geneCards: GeneCard[];
+  pageMax: number;
+}
+
 const GeneBrowser: React.FC = () => {
-  const [pageView, setPageView] = React.useState<{
-    geneCards: GeneCard[];
-    pageMax: number;
-  }>({
+  /* GENE DETAILS TABLE */
+
+  const modalSize = useBreakpointValue({ base: 'full', md: 'xl', lg: '2xl' });
+
+  const {
+    isOpen: isGeneDetailsOpen,
+    onOpen: onGeneDetailsOpen,
+    onClose: onGeneDetailsClose,
+  } = useDisclosure();
+
+  const refGeneDetailsAccession = React.useRef<string>('');
+
+  const showGeneDetails = (accessionId: string) => () => {
+    refGeneDetailsAccession.current = accessionId;
+    onGeneDetailsOpen();
+  };
+
+  /* GENE BROWSER VIEW */
+
+  const [pageView, setPageView] = React.useState<PageView>({
     geneCards: [],
     pageMax: 1,
   });
 
-  /* AUXILIARY */
-
-  const computeGeneView = React.useCallback(
-    (
-      accessionId: string,
-      countView: number,
-      pageNum: number
-    ): {
-      geneCards: GeneCard[];
-      pageMax: number;
-    } => {
+  const computePageView = React.useCallback(
+    (accessionId: string, countView: number, pageNum: number): PageView => {
       // Retrieve gene information matching the search parameters (empty search matches all)
       const regexp = new RegExp(escapeRegExp(accessionId), 'i');
       const matchingResults = dataTable.rowNames.reduce(
@@ -68,94 +92,30 @@ const GeneBrowser: React.FC = () => {
       // Calculate the number of pages according to the current display options
       const pageMax = Math.ceil(matchingResults.length / countView) || 1;
 
-      // console.log({ geneCards, pageMax });
-
-      // setPageView({ geneCards, pageMax });
       return { geneCards, pageMax };
     },
     []
   );
 
-  /* LYFECYCLE */
-
   React.useEffect(
     function loadInitialGeneView() {
-      const pageView = computeGeneView('', 20, 1);
+      const pageView = computePageView('', 20, 1);
       setPageView(pageView);
     },
-    [computeGeneView]
+    [computePageView]
   );
 
-  /* EVENTS */
-
-  // /**
-  //  * Update the _countDisplay_ state property.
-  //  * @param {React.ChangeEvent<HTMLSelectElement>} event
-  //  */
-  // const onDisplayCountSelect = (event) => {
-  //   const countView = event.target.value;
-  //   this.setState({ countView });
-  // };
-
-  // /**
-  //  * Re-evaluate the captions result, including only accessions ids matching fully or
-  //  * partially the current _searchId_ state property. This event also re-calculates the
-  //  * _countOffset_ and _countView_ state properties.
-  //  * @param {React.KeyboardEvent} event
-  //  */
-  // const onGeneSearchSubmit = (event) => {
-  //   if (event.key === 'Enter') {
-  //     this.setState({ pageOffset: 1, searchId: event.target.value });
-  //   }
-  // };
-
-  // /**
-  //  * Update the _pageOffset_ state property.
-  //  * @param {React.ChangeEvent<HTMLInputElement} event
-  //  */
-  // onPageOffsetChange = (event) => {
-  //   let pageOffset = event.target.value;
-
-  //   if (pageOffset <= 0 || pageOffset > this.state.pageMax) {
-  //     pageOffset = this.state.pageOffset;
-  //   }
-
-  //   this.setState({ pageOffset });
-  // };
-
   const onBrowserFormSubmit: BrowserFormSubmitHandler = (values, actions) => {
-    const pageView = computeGeneView(
+    const pageView = computePageView(
       values.accessionId,
       values.countView,
       values.pageNum
     );
 
-    console.log(pageView);
-
     setPageView(pageView);
 
     actions.setSubmitting(false);
   };
-
-  // /**
-  //  * Toggle the gene details modal visibility.
-  //  * @param {React.MouseEvent<HTMLDivElement>} event
-  //  */
-  // selectGene = (selectedGene) => {
-  //   let selectedGeneCounts;
-  //   if (selectedGene) {
-  //     const table = dataTable.getRowAsMap(selectedGene, true);
-  //     selectedGeneCounts = [...table.entries()].map(
-  //       ([[group, sample, replicate], count]) => ({
-  //         group,
-  //         sample,
-  //         replicate,
-  //         count,
-  //       })
-  //     );
-  //   }
-  //   this.setState({ selectedGene, selectedGeneCounts });
-  // };
 
   return (
     <Box backgroundColor="white" padding={6}>
@@ -189,22 +149,32 @@ const GeneBrowser: React.FC = () => {
           key={accession}
           padding={5}
           tabIndex={0}
+          onDoubleClick={showGeneDetails(accession)}
         />
       ))}
 
-      {/* {this.state.selectedGene && (
-        <AppModal
-          className="flex flex-col w-full h-full md:w-5/6 md:h-5/6 lg:w-3/4 2xl:w-1/2"
-          title={this.state.selectedGene}
-          showModal={this.state.selectedGene}
-          hideModal={() => this.selectGene()}
-        >
-          <GeneDetails
-            countUnit={settings.gxpSettings.unit}
-            geneCounts={this.state.selectedGeneCounts}
-          />
-        </AppModal>
-      )} */}
+      <Modal
+        isOpen={isGeneDetailsOpen}
+        onClose={onGeneDetailsClose}
+        size={modalSize}
+      >
+        <ModalOverlay />
+        <ModalContent margin={0} overflow="auto" rounded="none">
+          <ModalHeader color="orange.600">
+            {refGeneDetailsAccession.current}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <GeneDetails accessionId={refGeneDetailsAccession.current} />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="orange" mr={3} onClick={onGeneDetailsClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
