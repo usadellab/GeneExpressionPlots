16067a4cda08581f4eaf89dbc6f84e1815caf495
David Velasco <davvelsan@gmail.com>
2021-07-01 10:49:54 +0200

    feat: change the default data route to data-files

--- src/pages/data/data-routes.tsx
+++ src/pages/data/data-routes.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import DataFiles from './data-files';
-import DataHome from './data-home';
 
 const DataRoutes: React.FC = () => {
   const { path } = useRouteMatch();
 
   return (
     <Switch>
-      <Route exact path={path} component={DataHome} />
-      <Route exact path={path + '/files'} component={DataFiles} />
+      <Route exact path={path} component={DataFiles} />
     </Switch>
   );
 };
