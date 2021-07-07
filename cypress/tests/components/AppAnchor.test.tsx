e592a049008bed7ff183c87997e16920edf6d08c
coeit <cbeiteneuer@gmail.com>
2021-07-07 11:19:07 +0200

    chore: remove AppAnchor test. AppAnchor no longer exists

--- cypress/tests/components/AppAnchor.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { mount } from '@cypress/react';
-import AppAnchor from '@/components/AppAnchor';
-
-it('renders learn react link', () => {
-  mount(<AppAnchor href="/test/path">Gene Expression Plots</AppAnchor>);
-  cy.get('a').contains('Gene Expression Plots');
-});
