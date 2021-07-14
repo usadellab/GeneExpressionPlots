bf316fce2786e74fd3b767ead782203667ad2c5c
coeit <36563701+coeit@users.noreply.github.com>
2021-07-14 10:33:17 +0200

    feat: webworkers for heatmap and pca (#106)
    * refactor: move webworker utilites
    
    * refactor: heatmap + pca use webworker
    
    * feat: heatmap webworker
    
    * feat: pca webworker
    
    * chore: remove debug logs
    refactor: separate webworker functions to separate modules
    
    * chore: mark unsued functions in dataframe as deprecated

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -7,3 +7,34 @@ export function setToColors(set: Set<string>): Record<string, string> {
   const colorMap: Record<string, string> = Object.fromEntries(valueColorZip);
   return colorMap;
 }
+
+/**
+ * Get an array of colors for each unique dataframe subheader.
+ * - A subheader is a partial column name composed of `n` chunks.
+ * - Each chunk is the corresponding part of a multiheader column name.
+ *
+ * Because subheaders cannot be guaranteed to be unique, matching subheaders
+ * will also have matching colors.
+ *
+ * @param n number of chunks to use when parsing multi-headers
+ * @returns an array of hexadecimal color codes
+ */
+export function getSubheaderColors(
+  n: number,
+  colNames: string[],
+  multiHeader: string
+): string[] {
+  // Get the subheader combinations of `n` chunks length
+  const subHeaders = colNames.map((colName) => {
+    const chunks = colName.split(multiHeader);
+    return chunks.slice(0, n).join(multiHeader);
+  });
+
+  // Get a object of unique chunk to color pairs
+  const colorsMap = setToColors(new Set<string>(subHeaders));
+
+  // Assign subheader colors
+  const colors = subHeaders.map((head) => colorsMap[head]);
+
+  return colors;
+}
