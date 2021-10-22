d3ea338869a81e238c4d0d731c21b56c141d31b7
coeit <cbeiteneuer@gmail.com>
2021-10-22 10:55:56 +0200

    fix: typescript error for d3.mean() and d3.deviation()

--- src/utils/plots/multi-gene-bar.ts
+++ src/utils/plots/multi-gene-bar.ts
@@ -22,8 +22,8 @@ const multiGeneBarData = (
         if (groupSamplePlotData) {
           x[0].push(groupName);
           x[1].push(sampleName);
-          y.push(mean(groupSamplePlotData));
-          error_y.push(deviation(groupSamplePlotData));
+          y.push(mean(groupSamplePlotData) as number);
+          error_y.push(deviation(groupSamplePlotData) as number);
         }
       });
     });
