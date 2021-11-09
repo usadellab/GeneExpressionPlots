35ca5cacbf5a2e40386416444af3a1ec3ae9c3f9
coeit <cbeiteneuer@gmail.com>
2021-11-09 17:18:23 +0100

    feat: update mapman form / plot

--- src/pages/plots/components/mapman-form.tsx
+++ src/pages/plots/components/mapman-form.tsx
@@ -1,12 +1,10 @@
-import { Formik, Form, FieldValidator, FormikHelpers } from 'formik';
+import { Formik, Form, FormikHelpers } from 'formik';
 import React from 'react';
 import FormikField from '@/components/formik-field';
-import FormikFile from '@/components/formik-file';
 import { Box, Button, Flex } from '@chakra-ui/react';
 import { FocusableElement } from '@chakra-ui/utils';
 import FormikSelect from '@/components/formik-select';
-import { infoTable } from '@/store/data-store';
-import { values } from 'mobx';
+import { dataTable, infoTable } from '@/store/data-store';
 
 export type MapManFormSubmitHandler = (
   values: MapManFormAttributes,
@@ -19,53 +17,56 @@ export interface MapManFormProps {
   onSubmit: MapManFormSubmitHandler;
 }
 
-interface MapManFormAttributes {
+export interface MapManFormAttributes {
   infoTableColumn: string;
   infoTableColumnSep: string;
   template: string;
+  valuesFrom: string;
+  colorScale: 'sequential' | 'linear';
+  sample?: string;
 }
 
 const templates = [
   {
     label: 'Amino acid metabolism',
-    value: 'X4.3_Amino_acid_metabolism_R3.0.svg',
+    value: 'X4.3_Amino_acid_metabolism_R3.0',
   },
   {
     label: 'Carbohydrate metabolism',
-    value: 'X4.3_Carbohydrate_metabolism_R3.0.svg',
+    value: 'X4.3_Carbohydrate_metabolism_R3.0',
   },
   {
     label: 'Cellular respiration',
-    value: 'X4.3_Cellular_respiration_R3.0.svg',
+    value: 'X4.3_Cellular_respiration_R3.0',
   },
-  { label: 'Coenzyme metabolism', value: 'X4.3_Coenzyme_metabolism_R3.0.svg' },
-  { label: 'GA synthesis', value: 'X4.3_GA_synthesis_R3.0.svg' },
-  { label: 'JA synthesis', value: 'X4.3_JA_synthesis_R3.0.svg' },
-  { label: 'Lignin', value: 'X4.3_Lignin_R3.0.svg' },
-  { label: 'Lipid metabolism', value: 'X4.3_Lipid_metabolism_R3.0.svg' },
-  { label: 'Metabolism overview', value: 'X4.3_Metabolism_overview_R3.0.svg' },
+  { label: 'Coenzyme metabolism', value: 'X4.3_Coenzyme_metabolism_R3.0' },
+  { label: 'GA synthesis', value: 'X4.3_GA_synthesis_R3.0' },
+  { label: 'JA synthesis', value: 'X4.3_JA_synthesis_R3.0' },
+  { label: 'Lignin', value: 'X4.3_Lignin_R3.0' },
+  { label: 'Lipid metabolism', value: 'X4.3_Lipid_metabolism_R3.0' },
+  { label: 'Metabolism overview', value: 'X4.3_Metabolism_overview_R3.0' },
   {
     label: 'Nucleotide metabolism',
-    value: 'X4.3_Nucleotide_metabolism_R3.0.svg',
+    value: 'X4.3_Nucleotide_metabolism_R3.0',
   },
-  { label: 'Photosynthesis', value: 'X4.3_Photosynthesis_R3.0.svg' },
+  { label: 'Photosynthesis', value: 'X4.3_Photosynthesis_R3.0' },
   {
     label: 'Polyamine metabolism',
-    value: 'X4.3_Polyamine_metabolism_R3.0.svg',
+    value: 'X4.3_Polyamine_metabolism_R3.0',
   },
   {
     label: 'Secondary metabolism',
-    value: 'X4.3_Secondary_metabolism_R3.0.svg',
+    value: 'X4.3_Secondary_metabolism_R3.0',
   },
   {
     label: 'Tetrapyrrole biosynthesis',
-    value: 'X4.3_Tetrapyrrole_biosynthesis_R3.0.svg',
+    value: 'X4.3_Tetrapyrrole_biosynthesis_R3.0',
   },
   {
     label: 'Tocopherol biosynthesis',
-    value: 'X4.3_Tocopherol_biosynthesis_R3.0.svg',
+    value: 'X4.3_Tocopherol_biosynthesis_R3.0',
   },
-  { label: 'Vesicle trafficking', value: 'X4.3_Vesicle_trafficking_R3.0.svg' },
+  { label: 'Vesicle trafficking', value: 'X4.3_Vesicle_trafficking_R3.0' },
 ];
 
 const MapManForm: React.FC<MapManFormProps> = (props) => {
@@ -74,7 +75,10 @@ const MapManForm: React.FC<MapManFormProps> = (props) => {
       initialValues={{
         infoTableColumn: infoTable.colNames[0],
         infoTableColumnSep: ',',
-        template: 'X4.3_Amino_acid_metabolism_R3.0.svg',
+        template: 'X4.3_Amino_acid_metabolism_R3.0',
+        valuesFrom: infoTable.colNames[0],
+        colorScale: 'linear',
+        sample: dataTable.sampleGroupsAsArray[0],
       }}
       validateOnBlur={false}
       onSubmit={props.onSubmit}
@@ -85,7 +89,7 @@ const MapManForm: React.FC<MapManFormProps> = (props) => {
             controlProps={{
               marginTop: '1rem',
             }}
-            label="Info table column"
+            label="MapMan Info table column"
             name="infoTableColumn"
             options={infoTable.colNames.map((colName) => ({
               value: colName,
@@ -93,24 +97,67 @@ const MapManForm: React.FC<MapManFormProps> = (props) => {
             }))}
             tooltip="Select a column from your gene info table to test enrichment for"
           />
+
           <FormikField
             controlProps={{
               as: 'p',
               marginTop: '1rem',
             }}
             initialFocusRef={props.initialFocusRef}
-            label="Info table column separator"
+            label="MapMan Info table column separator"
             name="infoTableColumnSep"
             isRequired
           />
+
+          <FormikSelect
+            controlProps={{
+              marginTop: '2rem',
+            }}
+            label="Values from"
+            name="valuesFrom"
+            options={infoTable.colNames.reduce(
+              (acc, colName) => {
+                acc.push({ value: colName, label: colName });
+                return acc;
+              },
+              [{ value: 'expressionValue', label: 'Mean expression value' }]
+            )}
+          />
+          {formProps.values.valuesFrom == 'expressionValue' && (
+            <FormikSelect
+              controlProps={{
+                marginTop: '1rem',
+              }}
+              label="Sample"
+              name="sample"
+              options={dataTable.sampleGroupsAsArray.map((sample) => ({
+                label: sample,
+                value: sample,
+              }))}
+            />
+          )}
+
           <FormikSelect
             controlProps={{
               marginTop: '1rem',
             }}
-            label="MapMan Templates"
+            label="Color Scale"
+            name="colorScale"
+            options={[
+              { value: 'linear', label: 'linear' },
+              { value: 'sequential', label: 'sequential [-1,1]' },
+            ]}
+          />
+
+          <FormikSelect
+            controlProps={{
+              marginTop: '2rem',
+            }}
+            label="MapMan Template"
             name="template"
             options={templates}
           />
+
           <Flex as="p" paddingY={6} justifyContent="flex-end">
             <Button onClick={props.onCancel} colorScheme="red" variant="ghost">
               Cancel
--- src/pages/plots/components/mapman-plot.tsx
+++ src/pages/plots/components/mapman-plot.tsx
@@ -1,82 +1,81 @@
-import React, { useRef, useLayoutEffect, useState } from 'react';
+import React, { useRef, useLayoutEffect } from 'react';
 import * as d3 from 'd3';
 
 import PlotContainer from './plot-container';
 import { GxpMapMan } from '@/types/plots';
 import { dataTable, infoTable } from '@/store/data-store';
 import {
+  getBlockformat,
   getCoordinates,
   getMapManBins,
-  getXmlBins,
-  getXmlRecursive,
-  getXml_x_yCords,
   parseXmlData,
 } from '@/utils/plots/mapman-xml-domparser';
-import { BiAlignMiddle } from 'react-icons/bi';
-import { removeObserver } from 'mobx/dist/internal';
-import { readFile } from '@/utils/parser';
 
 import { interpolateRdBu } from 'd3-scale-chromatic';
 import { scaleSequential } from 'd3-scale';
+import { scaleLinear } from '@visx/scale';
 
-// import { getDataAreaXml } from '@/utils/plots/mapman-xml-domparser';
+const gradient0 = '#f33d15';
+const gradient1 = '#b4fbde';
 
-// interface dataArea {
-//   x: number;
-//   y: number;
-//   identifiers: identifier[];
-// }
 const SIZE = 5;
 
-interface identifier {
-  id: string;
-  recursive: boolean;
-}
+type MapManPlotProps = GxpMapMan;
 
-const colorScale = scaleSequential([-1, 1], interpolateRdBu);
-
-const MapManPlot: React.FC<GxpMapMan> = (props) => {
+const MapManPlot: React.FC<MapManPlotProps> = (props) => {
   const ref = useRef(null);
   const svgRef = useRef(null);
   props.template;
-  console.log(
-    infoTable.getGenesForMapManBin('MapMan-Bins', ',', '1.3.4', true)
-  );
 
-  const [xmlDocument, setxmlDocument] = useState<Document>();
+  const [group, sample] = props.sample
+    ? props.sample.split(dataTable.config.multiHeader)
+    : [undefined, undefined];
+
+  const rectValues =
+    props.valuesFrom === 'expressionValue' && group && sample
+      ? dataTable.getMapManMeanValues(group, sample)
+      : infoTable.getColumn(props.valuesFrom);
+
+  const maxValue = d3.max(Object.values(rectValues));
+  const minValue = d3.min(Object.values(rectValues));
+
+  const colorScale =
+    props.colorScale === 'sequential'
+      ? scaleSequential([-1, 1], interpolateRdBu)
+      : scaleLinear<string>({
+          range: [gradient1, gradient0],
+          domain: [minValue, maxValue],
+        });
+
   useLayoutEffect(() => {
-    const svgMaster = d3.select(ref.current);
-    console.log({ svgMaster });
     const svg = d3
       .select(svgRef.current)
-      .attr('width', '1024')
-      .attr('height', '800');
+      .attr('width', '100%')
+      .attr('height', '100%')
+      .attr('preserveAspectRatio', 'xMinYMin meet')
+      .attr('viewBox', '0 0 1024 800')
+      .classed('main-svg', true);
 
-    d3.xml(`mapman-templates/${props.template}`).then((data) => {
-      // console.log({ data });
-      svg.node()?.append(data.documentElement);
+    d3.xml(`mapman-templates/${props.template}.svg`).then((data) => {
+      (svg.node() as any).append(data.documentElement);
 
-      parseXmlData('mapman-templates/X4.3_Metabolism_overview_R3.0.xml').then(
+      parseXmlData(`mapman-templates/${props.template}.xml`).then(
         (xmlDocument) => {
           const svgViz = d3
             .select(svgRef.current)
             .append('g')
-            .attr('id', 'Viz layer');
-          const bins = xmlDocument.getElementsByTagName('Identifier');
-          // console.log({ xmlDocument, bins });
+            .attr('id', 'viz-layer');
 
           const dataAreas = xmlDocument.querySelectorAll('DataArea');
-          // console.log({ dataAreas });
 
-          dataAreas.forEach((dataArea) => {
-            // console.log(dataArea.getElementsByTagName('Identifier'));
+          dataAreas.forEach((dataArea: Element) => {
             const { x, y } = getCoordinates(dataArea);
+            const { bformat, fnumber } = getBlockformat(dataArea);
             const bins = getMapManBins(dataArea);
-            // console.log({ x, y, bins });
 
             const geneIds = infoTable.getGenesForMapManBin(
-              'MapMan-Bins',
-              ',',
+              props.infoTableColumn,
+              props.infoTableColumnSep,
               bins[0].id,
               bins[0].recursive
             );
@@ -85,28 +84,37 @@ const MapManPlot: React.FC<GxpMapMan> = (props) => {
             let yOffset = 0;
 
             geneIds.forEach((geneId) => {
-              const rectValue = infoTable.getColumnValue(
-                geneId,
-                'log-fold change'
-              );
+              const rectValue = rectValues[geneId];
 
-              console.log({ rectValue });
-              // console.log(colorScale(rectValue));
+              const xValue =
+                bformat === 'x'
+                  ? x + (xOffset % fnumber) * SIZE
+                  : x + xOffset * SIZE;
+              const yValue =
+                bformat === 'y'
+                  ? y + (yOffset % fnumber) * SIZE
+                  : y + yOffset * SIZE;
 
               svgViz
                 .append('rect')
-                .attr('x', x + (xOffset % 5) * SIZE)
-                .attr('y', y + yOffset * SIZE)
+                .attr('x', xValue)
+                .attr('y', yValue)
                 .attr('height', SIZE)
                 .attr('width', SIZE)
                 .style('stroke', 'lightgray')
-                .style('fill', colorScale(parseFloat(rectValue)))
+                .style('fill', colorScale(parseFloat(rectValue as string)))
                 .append('title')
                 .text(bins[0].id + ': ' + geneId + ' - ' + rectValue);
 
-              xOffset++;
-              if (xOffset != 0 && xOffset % 5 === 0) {
+              bformat === 'x' ? xOffset++ : yOffset++;
+              if (bformat === 'x' && xOffset != 0 && xOffset % fnumber === 0) {
                 yOffset++;
+              } else if (
+                bformat === 'y' &&
+                yOffset != 0 &&
+                yOffset % fnumber === 0
+              ) {
+                xOffset++;
               }
             });
           });
@@ -116,15 +124,23 @@ const MapManPlot: React.FC<GxpMapMan> = (props) => {
   }, []);
   return (
     <PlotContainer
-      justifyContent="center"
       figureRef={ref}
       position="relative"
-      height={1200}
+      height={800}
       plotType="heatmap"
       status="idle"
       id={props.id}
+      overflow="auto"
     >
-      <svg ref={svgRef} />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          height: '100%',
+        }}
+      >
+        <svg ref={svgRef} />
+      </div>
     </PlotContainer>
   );
 };
