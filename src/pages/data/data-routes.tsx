4956c5fd6147685b4bbde55c7715a94d217e9bd2
coeit <36563701+coeit@users.noreply.github.com>
2021-11-18 11:37:09 +0100

    Chore: update packages (#135)
    * chore: update all packages to latest
    
    * fix: react-router-dom updated API
    see https://reactrouter.com/docs/en/v6/upgrading/v5
    
    * fix: mobx warning, use runInAction for async actions

--- src/pages/data/data-routes.tsx
+++ src/pages/data/data-routes.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import DataFiles from './data-files';
 
 const DataRoutes: React.FC = () => {
-  const { path } = useRouteMatch();
-
   return (
-    <Switch>
-      <Route exact path={path} component={DataFiles} />
-    </Switch>
+    <Routes>
+      <Route path="/" element={<DataFiles />} />
+    </Routes>
   );
 };
 
