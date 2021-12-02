ce1d6f170878c3fe5c2022502e3157b03626c472
coeit <cbeiteneuer@gmail.com>
2021-12-02 14:50:28 +0100

    fix: enrichment analysis test

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -23,6 +23,3 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.Commands.add('dataCy', (value) => {
-  return cy.get(`[data-cy=${value}]`);
-});
